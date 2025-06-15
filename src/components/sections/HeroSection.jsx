b4b7014168b7a2a38540d863d4adcf9ebbbcb4cd
Angel Dev <75772159+Angel-999@users.noreply.github.com>
2025-06-14 22:59:18 -0700

    1.0

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -40,8 +40,8 @@ const HeroSection = () => {
                 Learn more
               </Button>
             </Link>
-            <Link to="/register">
-              <Button 
+            <Link to="https://app.incashy.com">
+              <Button
                 className="px-8 py-3 text-lg bg-gradient-to-r from-green-500 to-green-500 hover:from-green-600 hover:to-green-600"
               >
                 Join now
